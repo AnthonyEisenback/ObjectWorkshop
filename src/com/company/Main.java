00c5503df8bf5b8c33e0cbcdd89595858e03eb66
AnthonyEisenback <anthonyeisenback@Interapts-MacBook-Pro-3.local>
2018-01-31 16:14:37 -0500

    Added cat class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+
+        Cat cat = new Cat(4,true, "Poptart");
+
+        System.out.println(cat.getColor());
+        System.out.println(cat.getLegs());
+        System.out.println(cat.isFur());
+
     }
 }
